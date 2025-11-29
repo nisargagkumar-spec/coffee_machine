55b9c6e90c3252e2b777a31671e5c348d9267bf6
nisargagkumar-spec <nisarga.gkumar@gmail.com>
2025-11-29 23:37:13 +0100

    Feature added-accepts coins and return change based on the user's coffee

--- main.py
+++ main.py
@@ -33,25 +33,33 @@ resources = {
 print("Welcome to the Coffee Cafe!")
 while True:
     user_coffee = input("What would you like? (espresso/latte/cappuccino):")
-    if user_coffee=="espresso":
-        print(f"Here is your {user_coffee}")
-    elif user_coffee=="latte":
-        print(f"Here is your {user_coffee}")
-    elif user_coffee=="cappucino":
-        print(f"Here is your {user_coffee}")
-    elif user_coffee=="report":
-        print(f"Here is your {resources}")
+    if user_coffee=="report":
+        for items in resources:
+            res=resources[items]
+            print(f"{items}-{res}")
     elif user_coffee=="off":
         print("Machine shut down")
+        break
     else:
-        print("Select from the above 3 coffess only!")
-
-    def make_coffee(user_choice):
         per_coffee = MENU[user_coffee]["ingredients"]
         for items in per_coffee:
-            cost_per_coffee = MENU[user_coffee]["cost"]
             required_resources = per_coffee[items]
-            print(items,"-",required_resources)
-        print(f"Your cost for the {user_choice} is:{cost_per_coffee}")
+            print(items, "-", required_resources)
 
-    make_coffee(user_coffee)
+        def makecoffee(user_choice):
+            cost_per_coffee = MENU[user_coffee]["cost"]
+            print(f"Your cost for the {user_choice} is:{cost_per_coffee}")
+            quarters=float(input("How many quaters??"))
+            dimes=float(input("How many dimes?"))
+            nickles=float(input("How many nickles"))
+            pennies=float(input("How many pennies"))
+            total_money=quarters*0.25+dimes*0.10+nickles*0.05+pennies*0.01
+            if total_money==cost_per_coffee:
+                print(f"Here is your {user_coffee}")
+            elif total_money>cost_per_coffee:
+                refund_money=round(total_money-cost_per_coffee,2)
+                print((f"Here is your {user_coffee} and the change of {refund_money}"))
+            else:
+                print(f"The money is not sufficent and the cost of {user_coffee} is {cost_per_coffee}")
+
+        makecoffee(user_coffee)
